9fbc172c1ebf6bca69841c5db54780d0ec671bac
Artemii <artem-lukichev@bk.ru>
2023-02-21 19:08:37 +0300

    fix

--- golang/db.go
+++ golang/db.go
@@ -6,7 +6,10 @@ import (
 	"github.com/ydb-platform/ydb-go-sdk/v3"
 	"github.com/ydb-platform/ydb-go-sdk/v3/sugar"
 	"github.com/ydb-platform/ydb-go-sdk/v3/table"
+	"github.com/ydb-platform/ydb-go-sdk/v3/table/options"
+	"github.com/ydb-platform/ydb-go-sdk/v3/table/types"
 	yc "github.com/ydb-platform/ydb-go-yc"
+	"reflect"
 	"time"
 )
 
@@ -20,23 +23,25 @@ type Config struct {
 	Database string
 }
 
-func main() {
-	var cfg Config
-	cfg.Endpoint = "ydb.serverless... your endpoint without grpcs://"
-	cfg.Database = "/ru... your database"
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
-	defer cancel()
-	db, err := ydb.Open(ctx,
-		sugar.DSN(cfg.Endpoint, cfg.Database, true),
-		yc.WithInternalCA(),
-		yc.WithServiceAccountKeyFileCredentials("./authorized_key.json"),
+func createTableExample(ctx context.Context, db ydb.Connection, cfg Config) {
+	err := db.Table().Do(ctx,
+		func(ctx context.Context, s table.Session) (err error) {
+			return s.CreateTable(ctx, cfg.Database+"/series",
+				options.WithColumn("series_id", types.TypeUint64), // not null column
+				options.WithColumn("title", types.Optional(types.TypeUTF8)),
+				options.WithColumn("release_date", types.Optional(types.TypeDate)),
+				options.WithPrimaryKeyColumn("series_id"),
+			)
+		},
 	)
 	if err != nil {
-		fmt.Println(err)
 		panic(err)
 	}
-	err = db.Table().Do(ctx, func(ctx context.Context, s table.Session) error {
-		query := `SELECT * FROM departments`
+}
+
+func SelectExample(ctx context.Context, db ydb.Connection, cfg Config) {
+	err := db.Table().Do(ctx, func(ctx context.Context, s table.Session) (err error) {
+		query := `SELECT * FROM series`
 		_, res, err := s.Execute(ctx, table.DefaultTxControl(), query, table.NewQueryParameters())
 		if err != nil {
 			return err
@@ -58,7 +63,31 @@ func main() {
 		return nil
 	})
 	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	var cfg Config
+	cfg.Endpoint = "ydb.serverless... your endpoint without grpcs://"
+	cfg.Database = "/ru... your database"
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+	fmt.Println()
+	db, err := ydb.Open(ctx,
+		sugar.DSN(cfg.Endpoint, cfg.Database, true),
+		yc.WithInternalCA(),
+		yc.WithServiceAccountKeyFileCredentials("./authorized_key.json"),
+	)
+	fmt.Println(reflect.TypeOf(db))
+	if err != nil {
 		fmt.Println(err)
+		panic(err)
+	}
+	// create table
+	//createTableExample(ctx, db, cfg)
+	SelectExample(ctx, db, cfg)
+	if err != nil {
 		panic(err)
 	}
 	defer func() {
